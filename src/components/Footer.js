469a44372a0d0db7b5020edcfbce46d13fd6da38
Gabe <41127686+Zidious@users.noreply.github.com>
2020-07-12 15:30:17 +0100

    Style Changes
    - Removed text on projects, repo and languages, too cluttered
    - Added joke, powered by cookies
    - Changed form title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ function Footer() {
     <footer className='flex-grow w-full   inset-x-0 bottom-0 text-center p-1'>
       <Socials />
       <div className='py-1'>&copy; Gabe Olesen</div>
+      <div>Powered by 🍪</div>
     </footer>
   );
 }
